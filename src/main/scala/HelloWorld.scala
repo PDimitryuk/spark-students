ded095651e4308ae7f083692d60bc88ab32420c0
Pavel Dimitryuk <pdimitryuk@gmail.com>
2018-04-21 16:51:13 +0300

    Fix. HADOOP utils for windows

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,16 @@
+import java.io.File
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
  
 object HelloWorld extends App {
+
+  //for windows
+  val classLoader: ClassLoader = getClass.getClassLoader
+  val file: File = new File(classLoader.getResource("hadoop/bin/winutils.exe").getFile)
+  val hadoopPath = file.getAbsolutePath
+  System.setProperty("hadoop.home.dir", s"$hadoopPath\\..\\..")
 
   //initialise spark context
   val conf = new SparkConf()
